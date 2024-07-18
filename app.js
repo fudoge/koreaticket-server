1ba1da192387ba363cfefc53b13cf72d5a4353e1
fudoge <kchawono@naver.com>
2024-07-18 15:25:27 +0900

    main: added servicing with react app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const authRouter = require("./routes/authRoutes");
 const reviewBoardRouter = require("./routes/reviewRoutes");
@@ -10,11 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/build')));
 app.use('/auth', authRouter);
 app.use('/reviews', reviewBoardRouter);
 
 app.get('/', (req, res) => {
-    res.send("hello");
+    res.sendFile(path.join((__dirname, '/build/index.html')));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join((__dirname, '/build/index.html')));
 });
 
 app.listen(port, () => {
