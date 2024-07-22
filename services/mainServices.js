c83b8c5c0e71a31202c25981fb521d95868c578a
fudoge <kchawono@naver.com>
2024-07-22 10:05:59 +0900

    feature/main: mainService시간 로직 KST에 맞게  수정

--- services/mainServices.js
+++ services/mainServices.js
@@ -1,20 +1,29 @@
 const { Op } = require('sequelize');
-const { Match, Team, ReviewPost, ReviewImage } = require('../models/model');
+const { Match, Team, ReviewPost, ReviewImage, Stadium } = require('../models/model');
 const moment = require('moment');
 const now = moment();
 
 exports.returnInfos = async (req, res) => {
+    const time = new Date();
+    time.setHours(time.getHours() + 9);
+    const currentKST = time.toISOString();
+    time.setDate(time.getDate() + 1);
+    const tomorrowKST = time.toISOString();
+
     try {
         // 헤드라인 경기들 불러오기
         const headLineMatches = await Match.findAll({
+            attributes: ['matchTime', 'matchId'],
             where: {
                 matchTime: {
-                    [Op.lt]: now.add(24, 'hours').format(),
-                    [Op.gt]: now.format()
+                    [Op.gt]: currentKST,
+                    [Op.lt]: tomorrowKST
                 }
             }, include: [
-                Team
-            ]
+                    { model: Team, as: 'HomeTeam' },
+                    { model: Team, as: 'AwayTeam' },
+                    { model: Stadium }
+                ]
         });
 
         // 후기게시판 썸네일과 제목 불러오기
@@ -24,8 +33,11 @@ exports.returnInfos = async (req, res) => {
             limit: 4
         });
 
+        //TODO: 외부 API호출해서 순위표 불러오기
+        
+
         return res.status(200).json({ headLineMatches: headLineMatches, reviewPosts: reviewPosts });
     } catch (e) {
-        return res.status(500).json({error: e});
+        return res.status(500).json({error: e.message});
     }
 };
